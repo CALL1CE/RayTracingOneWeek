8b9f236f616144af2b292776ca4a5393401a563d
张召寒 <563638083@qq.com>
2024-01-02 14:49:22 +0800

    去掉了move，添加aabb头文件

--- RTWeekend/src/sphere.h
+++ RTWeekend/src/sphere.h
@@ -5,8 +5,9 @@
 
 class sphere : public hittable {
 public:
+	// Stationary Sphere
 	sphere(point3 _center, double _radius, shared_ptr<material> _material) 
-		: center(_center), radius(_radius), mat(_material){}
+		: center(_center), radius(_radius), mat(_material) {}
 
 	bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
 		vec3 oc = r.origin() - center;
@@ -39,7 +40,9 @@ public:
 	}
 
 private:
+
 	point3 center;
 	double radius;
 	shared_ptr<material> mat;
+
 };
